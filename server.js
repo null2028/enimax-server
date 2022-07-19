dabe2570fe15a8e23a11c1486612b74c010ad890
enimax-anime <soitchu@proton.me>
2022-07-19 14:10:42 -0700

    Removed unnecessary headers

--- server.js
+++ server.js
@@ -132,11 +132,6 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    next();
-});
 
 app.use(session({
     secret: process.env.secret,
